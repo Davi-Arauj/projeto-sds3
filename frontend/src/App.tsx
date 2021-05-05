dd378b0dad89872572df8b4070de841d9008ef22
Davi-Arauj <moreiradaavi@gmail.com>
2021-05-05 12:15:38 -0300

    Alteração no Asp.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,16 +16,16 @@ function App() {
             <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
-        </div>
-        <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <h5 className="text-center text-secondary">Todas  Vendas (%)</h5>
             <DonutChart />
           </div>
         </div>
+
         <div className="py-3">
           <h2 className="text-primary">Todas Vendas</h2>
         </div>
+
         <DataTable />
       </div>
       <Footer />
