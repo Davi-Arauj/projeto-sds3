0da1d7a4056e26d6f83c8511c5c8752608c50cef
Davi-Arauj <moreiradaavi@gmail.com>
2021-05-04 23:34:31 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Davi-Arauj" target="_blank" rel="noreferrer">Davi Araujo</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/davi-arauj" target="_blank" rel="noreferrer">@davi-arauj</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
