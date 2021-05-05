ad5a887999b9e3427e9afe06b542db32ac03720c
Davi-Arauj <moreiradaavi@gmail.com>
2021-05-05 12:57:47 -0300

    Alteração no nome do Instagran

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/Davi-Arauj" target="_blank" rel="noreferrer">Davi Araujo</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/davi-arauj" target="_blank" rel="noreferrer">@davi-arauj</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/davi_moreira28" target="_blank" rel="noreferrer">@davi_moreira28</a></small></p>
             </div>
         </footer>
     );
